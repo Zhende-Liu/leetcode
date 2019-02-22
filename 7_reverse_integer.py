226b9a7dc5ffd8567e4008789cce710c220b15b7
Zhende-Liu <45912039+Zhende-Liu@users.noreply.github.com>
2019-02-22 18:50:53 +1100

    update problem7

--- 7_reverse_integer.py
+++ 7_reverse_integer.py
@@ -6,3 +6,23 @@
 class Solution:
     def reverseInteger(self, x: 'int'):
         bit_list = []
+        q = 1
+        if x < 0:
+            q = -1
+            x = q * x
+        while x > 0:
+            n = x % 10
+            x = x // 10
+            bit_list.append(n)
+        out_num = 0
+        for i in bit_list:
+            out_num = out_num * 10 + i
+        print(q * out_num)
+        return  q * out_num
+
+if __name__ == '__main__':
+    solution = Solution()
+    x = 120
+    solution.reverseInteger(x)
+
+        
