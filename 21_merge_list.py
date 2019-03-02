a84c85b6a66c9d13f323cae7d3eec040820c086b
Zhende-Liu <45912039+Zhende-Liu@users.noreply.github.com>
2019-03-02 23:54:59 +1100

    add 21_merge_lsit.py

---
+++ 21_merge_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-02-28 23:19
+# @Author  : ZD Liu
+
+
+class Solution:
+    def merge_list(self,obj_list1, obj_list2):
+        i = 0
+        for j in lis2:
+            while i < len(obj_list1)
+                if lis2[j] >
+
+
+
+
+        return res_list
+
+
+if __name__ = "__main__":
+    solution = Solution()
+    lis1 = [1,2,5]
+    lis2 = [2,4,7]
+    res = solution.merge_list(lis1,lis2)
+    print(res)
+
